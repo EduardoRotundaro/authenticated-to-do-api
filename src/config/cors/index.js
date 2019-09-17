6eb394f882eb73a2a5b49768cbe615213be5e011
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-09-17 14:43:11 -0300

    Project structure

--- src/config/cors/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    OPTIONS: {
-        origin: '*',
-        methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }
-}
